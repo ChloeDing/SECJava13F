269297732d82ed666a1e8d8d664f139509a26abc
Ted-X220\Ted <yangxiongtan@gmail.com>
2017-02-14 23:58:29 -0700

    separate method for XML to HashMap and HashMap to File

--- src/SECF13Converter.java
+++ src/SECF13Converter.java
@@ -19,7 +19,7 @@ import org.w3c.dom.NodeList;
  * This one is used to convert SEC F13 XML files to excel (.xls) file with three
  * columes (consolidated company names, values and shares)
  * 
- * @author dliu
+ * @author dliu & tyang
  *
  */
 
@@ -30,20 +30,22 @@ public class SECF13Converter extends JFrame {
 	private JTextField f13linkAJLableJTextField;
 	private JTextField f13linkBJLableJTextField;
 	private JButton convertXMLtoEXCELJButton;
-	private JLabel resultMessageJLableA;
-	private JLabel resultMessageJLableB;
+	private JTextArea resultMessageJTextArea;
+
+	private LinkedHashMap<String, Double> companyValuePair;
+	private LinkedHashMap<String, Double> companySharePair;
 
 	public static void main(String[] args) {
-		
+
 		SECF13Converter application = new SECF13Converter();
 		application.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		
+
 	}
 
 	private SECF13Converter() {
-		
+
 		createUserInterface();
-		
+
 	}
 
 	private void createUserInterface() {
@@ -59,12 +61,12 @@ public class SECF13Converter extends JFrame {
 		f13linkAJLableJTextField = new JTextField();
 		f13linkAJLableJTextField.setBounds(150, 40, 300, 20);
 		contentPane.add(f13linkAJLableJTextField);
-		
+
 		f13linkBJLable = new JLabel();
 		f13linkBJLable.setBounds(40, 80, 90, 20);
 		f13linkBJLable.setText("URL for XML B:");
 		contentPane.add(f13linkBJLable);
-		
+
 		f13linkBJLableJTextField = new JTextField();
 		f13linkBJLableJTextField.setBounds(150, 80, 300, 20);
 		contentPane.add(f13linkBJLableJTextField);
@@ -79,16 +81,12 @@ public class SECF13Converter extends JFrame {
 			}
 
 		});
-		
-		resultMessageJLableA = new JLabel();
-		resultMessageJLableA.setBounds(40, 250, 500, 20);
-		resultMessageJLableA.setText("");
-		contentPane.add(resultMessageJLableA);
-		
-		resultMessageJLableB = new JLabel();
-		resultMessageJLableB.setBounds(40, 290, 500, 20);
-		resultMessageJLableB.setText("");
-		contentPane.add(resultMessageJLableB);
+
+		resultMessageJTextArea = new JTextArea();
+		resultMessageJTextArea.setBounds(80, 250, 350, 150);
+		resultMessageJTextArea.setText("");
+		resultMessageJTextArea.setEditable(false);
+		contentPane.add(resultMessageJTextArea);
 
 		setTitle("XML to EXCEL"); // set interface title
 		setSize(600, 600); // set window size
@@ -103,32 +101,35 @@ public class SECF13Converter extends JFrame {
 		// System.out.println("doc size: " + inputDocA.getNodeName());
 
 		try {
-			f13convert(inputDocA);
-			resultMessageJLableA.setText("SEC F13 XML A successfualy converted to excel (.xls) file with three columes (consolidated company names, values and shares).");
+			f13ConvertToHashMap(inputDocA);
+			f13HashMaptoFile("A");
+			resultMessageJTextArea.append(
+					"SEC F13 XML A successfualy converted to excel (.xls) file with three columes (consolidated company names, values and shares).");
 		} catch (Exception e) {
 			e.printStackTrace();
-			resultMessageJLableA.setText("SEC F13 XML A failed to converted to excel file.");
+			resultMessageJTextArea.append("SEC F13 XML A failed to converted to excel file.");
 			return;
 		}
-		
+
 		String xmlUrlStringB = f13linkBJLableJTextField.getText();
 		// download the xml file from given url
 		Document inputDocB = XMLUtil.getDocumentFromURL(xmlUrlStringB);
 		// System.out.println("doc size: " + inputDocB.getNodeName());
 
 		try {
-			f13convert(inputDocB);
-			resultMessageJLableB.setText("SEC F13 XML B successfualy converted to excel (.xls) file with three columes (consolidated company names, values and shares).");
+			f13ConvertToHashMap(inputDocB);
+			f13HashMaptoFile("B");
+			resultMessageJTextArea.append(
+					"\n" + "SEC F13 XML B successfualy converted to excel (.xls) file with three columes (consolidated company names, values and shares).");
 		} catch (Exception e) {
 			e.printStackTrace();
-			resultMessageJLableB.setText("SEC F13 XML B failed to converted to excel file.");
+			resultMessageJTextArea.append("\n" + "SEC F13 XML B failed to converted to excel file.");
 			return;
 		}
-		
-		
+
 	}
 
-	private void f13convert(Document inputDoc) throws Exception {
+	private void f13ConvertToHashMap(Document inputDoc) {
 		// in
 		Element root = inputDoc.getDocumentElement();
 
@@ -137,10 +138,10 @@ public class SECF13Converter extends JFrame {
 		// System.out.println("companyList: " + companyList.getLength());
 
 		// LinkedHashMap between the pair of company (the key) and value
-		LinkedHashMap<String, Long> companyValuePair = new LinkedHashMap<String, Long>();
+		companyValuePair = new LinkedHashMap<String, Double>();
 
 		// LinkedHashMap between the pair of company (the key) and shares
-		LinkedHashMap<String, Long> companySharePair = new LinkedHashMap<String, Long>();
+		companySharePair = new LinkedHashMap<String, Double>();
 
 		// loop through the company list to construct the company-value pair and
 		// the company-shares pair
@@ -156,20 +157,22 @@ public class SECF13Converter extends JFrame {
 			// System.out.println("shares: " + shares);
 
 			if (companyValuePair.get(nameOfIssuer) == null) {
-				companyValuePair.put(nameOfIssuer, Long.parseLong(value));
+				companyValuePair.put(nameOfIssuer, Double.parseDouble(value));
 			} else {
 				companyValuePair.put(nameOfIssuer, companyValuePair.get(nameOfIssuer) + Long.parseLong(value));
 			}
 
 			if (companySharePair.get(nameOfIssuer) == null) {
-				companySharePair.put(nameOfIssuer, Long.parseLong(shares));
+				companySharePair.put(nameOfIssuer, Double.parseDouble(shares));
 			} else {
 				companySharePair.put(nameOfIssuer, companySharePair.get(nameOfIssuer) + Long.parseLong(shares));
 			}
 		}
+	}
 
+	private void f13HashMaptoFile(String filename) throws Exception {
 		// out : write LinkedHashMap into excel file
-		File outFile = new File("ExcelFolder\\f13.xls");
+		File outFile = new File("ExcelFolder\\f13" + filename + ".xls");
 		XSSFWorkbook myWorkBook = new XSSFWorkbook();
 		XSSFSheet mySheet = myWorkBook.createSheet();
 		int rownum = mySheet.getLastRowNum();
@@ -190,10 +193,10 @@ public class SECF13Converter extends JFrame {
 			cellNameOfIssuer.setCellValue(name);
 
 			Cell cellValue = row.createCell(1);
-			cellValue.setCellValue((Long) companyValuePair.get(name));
+			cellValue.setCellValue((Double) companyValuePair.get(name));
 
 			Cell cellCount = row.createCell(2);
-			cellCount.setCellValue((Long) companySharePair.get(name));
+			cellCount.setCellValue((Double) companySharePair.get(name));
 		}
 
 		FileOutputStream os = new FileOutputStream(outFile);
